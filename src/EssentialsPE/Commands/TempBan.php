17f52c45f1cbe41a8fa871fb49553a2a56acf6b9
iksaku <iksaku@me.com>
2016-03-22 15:54:02 -0600

    Allow Temporal Bans for Offline users, Fixes weird "Empty" ban reason... Fixes #219

--- src/EssentialsPE/Commands/TempBan.php
+++ src/EssentialsPE/Commands/TempBan.php
@@ -30,6 +30,7 @@ class TempBan extends BaseCommand{
             $this->sendUsage($sender, $alias);
             return false;
         }
+        $name = array_shift($args);
         if(!($info = $this->getAPI()->stringToTimestamp(implode(" ", $args)))){
             $sender->sendMessage(TextFormat::RED . "[Error] Please specify a valid time");
             return false;
@@ -37,7 +38,7 @@ class TempBan extends BaseCommand{
         /** @var \DateTime $date */
         $date = $info[0];
         $reason = $info[1];
-        if(($player = $this->getAPI()->getPlayer(array_shift($args))) instanceof Player){
+        if(($player = $this->getAPI()->getPlayer($name)) instanceof Player){
             if($player->hasPermission("essentials.ban.exempt")){
                 $sender->sendMessage(TextFormat::RED . "[Error] " . $player->getDisplayName() . " can't be banned");
                 return false;
@@ -45,8 +46,8 @@ class TempBan extends BaseCommand{
                 $player->kick(TextFormat::RED . "Banned until " . TextFormat::AQUA . $date->format("l, F j, Y") . TextFormat::RED . " at " . TextFormat::AQUA . $date->format("h:ia") . (trim($reason) !== "" ? TextFormat::YELLOW . "\nReason: " . TextFormat::RESET . $reason : ""), false);
             }
         }
-        $sender->getServer()->getNameBans()->addBan($player->getName(), (trim($reason) !== "" ? $reason : null), $date, "essentialspe");
-        $this->broadcastCommandMessage($sender, "Banned player " . $player->getName() . " until " . $date->format("l, F j, Y") . " at " . $date->format("h:ia") . (trim($reason) !== "" ? TextFormat::YELLOW . " Reason: " . TextFormat::RESET . $reason : ""));
+        $sender->getServer()->getNameBans()->addBan(($player instanceof Player ? $player->getName() : $name), (trim($reason) !== "" ? $reason : null), $date, "essentialspe");
+        $this->broadcastCommandMessage($sender, "Banned player " . ($player instanceof Player ? $player->getName() : $name) . " until " . $date->format("l, F j, Y") . " at " . $date->format("h:ia") . (trim($reason) !== "" ? TextFormat::YELLOW . " Reason: " . TextFormat::RESET . $reason : ""));
         return true;
     }
 }
