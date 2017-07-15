0f5092eedd76806b85df421c496e947f672d31e8
Sandertv <st.ten.veldhuis@gmail.com>
2017-07-15 16:23:16 +0200

    PocketMine ALPHA7 updates, fixed #398, fixed some other small bugs.

--- src/EssentialsPE/Commands/Repair.php
+++ src/EssentialsPE/Commands/Repair.php
@@ -22,7 +22,7 @@ class Repair extends BaseCommand{
      * @param array $args
      * @return bool
      */
-    public function execute(CommandSender $sender, $alias, array $args): bool{
+    public function execute(CommandSender $sender, string $alias, array $args): bool{
         if(!$this->testPermission($sender)){
             return false;
         }
@@ -30,6 +30,7 @@ class Repair extends BaseCommand{
             $this->sendUsage($sender, $alias);
             return false;
         }
+        $a = "hand";
         if(isset($args[0])) {
             $a = strtolower($args[0]);
         }
